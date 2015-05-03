8395e1d0883d78f860b10a33bc299cbc08e1317d
Adrian Schlatter <schlatter@phys.ethz.ch>
2015-05-03 16:11:54 +0200

    Python3 +++++++
    Fixed some issues to make NoiseTools compatible with Python3.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+pack_name='NoiseTools'
+pack_version='0.3'
+pack_description='Tools for basic noise analysis'
+pack_author='Adrian Schlatter'
+
+setup(name=pack_name,version=pack_version,description=pack_description,
+      author=pack_author,py_modules=["NoiseTools"])
+
