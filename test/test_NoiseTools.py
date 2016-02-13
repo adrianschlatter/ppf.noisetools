1943735624df6fc7a97608fce643a388ea2922eb
Adrian Schlatter <schlatter@phys.ethz.ch>
2016-02-13 22:12:23 +0100

    Unittests Spectral Tools ++++++++++++++++++++++++
    Added unittests for spectrum, ispectrum, and powerspectrum.

--- test/test_NoiseTools.py
+++ test/test_NoiseTools.py
@@ -12,7 +12,7 @@ import NoiseTools as nt
 import numpy as np
 
 
-class TestNoiseTools(unittest.TestCase):
+class TestHelpers(unittest.TestCase):
 
     def test_crop2power2(self):
         x = np.arange(3)
@@ -24,6 +24,30 @@ class TestNoiseTools(unittest.TestCase):
         x = np.arange(5)
         self.assertTrue(np.all(nt.centercrop2power2(x) == x[0:-1]))
 
+
+class TestSpectralTools(unittest.TestCase):
+
+    def setUp(self):
+        phi = np.arange(16.) / 16. * 2 * np.pi
+        self.sig = np.cos(phi)
+        self.spec = nt.spectrum(self.sig)
+        self.sig_prime = nt.ispectrum(self.spec)
+        self.pspec = nt.powerspectrum(self.sig)
+
+    def test_spectrum(self):
+        expected = np.zeros(16.)
+        expected[[1, -1]] = 0.5
+        delta = self.spec - expected
+        self.assertAlmostEqual(np.sum(delta**2), 0)
+
+    def test_ispcetrum(self):
+        delta = self.sig_prime - self.sig
+        self.assertAlmostEqual(np.sum(delta**2), 0)
+
+    def test_powerspectrum(self):
+        expected = np.abs(self.spec[:len(self.spec) / 2])**2
+        delta = self.pspec - expected
+        self.assertAlmostEqual(np.sum(delta**2), 0)
 
 if __name__ == '__main__':
     unittest.main()
